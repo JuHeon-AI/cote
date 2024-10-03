f25612fc74475ff5a0f062b1d952ec7e1d8b59c1
JuHeonI <wngjs271@gmail.com>
2024-10-04 00:56:03 +0900

    1차원배열

--- cote.py
+++ cote.py
@@ -1,8 +1,12 @@
 import sys
 n = int(input())
 ans = []
+c =[]
+d =[]
 for i in range(n):
     a,b = map(int,sys.stdin.readline().rstrip().split())
+    c.append(a)
+    d.append(b)
     ans.append(a+b)
 for i in range(n):
-    print(f"Case #{i+1}: {ans[i]}")
+    print(f"Case #{i+1}: {c[i]} + {d[i]} = {ans[i]}")
